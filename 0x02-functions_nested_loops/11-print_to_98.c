abeb7a19ba2754b8377f61738abf7bf3e964ee79
Genesis Solomon <40307812+cy53rg@users.noreply.github.com>
2022-10-19 16:48:43 +0100

    Create 11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+/**
+ * C program to print all natural numbers from 1 to n using while loop
+ */
+
+#include "main.h"
+
+int main()
+{
+    int i, end;
+
+   
+
+    /*
+     * Print natural numbers from 0 to 98
+     */
+    i=0;
+    while(i<=98)
+    {
+        _putcar("%d\n", i);
+        i++;
+    }
+
+    return 0;
+}
