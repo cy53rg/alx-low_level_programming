0675c85e59d8975b6c89487f891fcfb2975b9686
Genesis Solomon <40307812+cy53rg@users.noreply.github.com>
2022-10-19 17:02:15 +0100

    Create 100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,11 @@
+#include "main.h"
+void times_table(void) 
+{
+  int n, i;
+  printf("Enter an integer: ");
+  scanf("%d", &n);
+  for (i = 1; i <= 10; ++i) {
+    printf("%d * %d = %d \n", n, i, n * i);
+  }
+  return 0;
+}
