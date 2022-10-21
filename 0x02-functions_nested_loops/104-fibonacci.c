d72afb76b2718a397754d28f7a597112d59ef0bf
Genesis Solomon <40307812+cy53rg@users.noreply.github.com>
2022-10-21 18:46:59 +0100

    Create 104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+void fibonnaci()
+{
+  int f1, f2, f3,i;
+  i = 1
+  f1=1;
+  f2=2;
+  f3=f1+f2;
+  printf("%d, %d /n" f1,f2);
+  while(i<=98)
+  {
+    printf("%d ,/n" f3);
+    f1=f2;
+    f2=f3;
+    
+    i++
+    }
+  return 0;
+ 
+}
